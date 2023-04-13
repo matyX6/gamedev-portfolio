8529bf77dc0139b45c6f6f09ee187a489f912b4f
mjambresic <matija.jambresic@yahoo.com>
2023-04-13 22:04:39 +0200

    bunners and some updates

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -28,7 +28,7 @@ export default [
         <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
         <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
     </div>
-    `, "#87CEEB", false, true),
+    `, "#6082B6", false, true),
     new ProjectData("project-2", "White Blocks", "img/projects/project-whiteblocks-icon.gif", `
     <div class="paragraph">
         <strong>Optimistic Chubby Blog</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
@@ -61,7 +61,7 @@ export default [
     <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
     <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
     </div>
-    `, "#383838", true, false),
+    `, "#40B5AD", true, false),
     new ProjectData("project-3", "Drawing Overload", "img/projects/project-3-icon.png", `
     <div class="paragraph">
         <strong>Drawing Overload</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
@@ -97,29 +97,30 @@ export default [
     `, "#383838"),
     new ProjectData("project-4", "Bunners", "img/projects/project-bunners-icon.gif", `
     <div class="paragraph">
-    <strong>Eugeneable</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-    <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@neo8iam">NEOSiAM 2020</a>.
+        <strong>Bunners</strong> was the first commercial project I have worked on. 
+        I designed and programmed the game from scratch and it was released on Google Play.
+        You play as a little bunny in this endless runner game and dodge obstacles to survive.
+    </div>
+    <div class="paragraph center">
+            <iframe class="youtube" src="https://www.youtube.com/embed/gQjPkl1dweU" frameborder="0" allowfullscreen></iframe>
     </div>
     
-    <div class="paragraph">
-        Main features :
+    <div class="paragraph">  
+    
+    Main features :
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+        <li>Fast & endless gameplay</li>
+        <li>In game currency</li>
+        <li>Shop with cosmetic items</li>
+        <li>Game customization/personalization</li>
         </ul>
     </div>
 
-    <div class="paragraph">
-        <div class="notice">
-        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
-        </div>
-    </div>
-
     <div class="paragraph center">
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Eugeneable Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Eugeneable Screenshot" />
+        <img class="pc-screenshot" src="/img/screenshots/ss-bunners-1.png" />
+        <img class="pc-screenshot" src="/img/screenshots/ss-bunners-2.png" />
+        <img class="pc-screenshot" src="/img/screenshots/ss-bunners-3.png" />
+        <img class="pc-screenshot" src="/img/screenshots/ss-bunners-4.png" />
     </div>
-    `, "#FFA500")
+    `, "#F4BB44")
 ];

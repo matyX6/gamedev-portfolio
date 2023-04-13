ccd81d55ac17dc010a9c96988a2452edac2c63dc
mjambresic <matija.jambresic@yahoo.com>
2023-04-13 22:46:21 +0200

    white blocks update

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -29,39 +29,35 @@ export default [
         <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
     </div>
     `, "#6082B6", false, true),
+    
     new ProjectData("project-2", "White Blocks", "img/projects/project-whiteblocks-icon.gif", `
     <div class="paragraph">
-        <strong>Optimistic Chubby Blog</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@adonyi-gabor-604571">Adonyi Gábor</a>.
+        After successfully releasing my first game, I designed and developed <strong>White Blocks</strong>.
+        The game was published on Google Play and App Store.
+        The main goal is to destroy as many blocks as possible while the game gets progressively harder.
     </div>
     <div class="paragraph center">
-        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
+        <iframe class="youtube" src="https://www.youtube.com/embed/JxALjkjmOGU" frameborder="0" allowfullscreen></iframe>
     </div>
 
     <div class="paragraph">
         Main features :
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+        <li>Fast & endless gameplay</li>
+        <li>Reaction skill test</li>
+        <li>Global leaderboards</li>
+        <li>Themes</li>
         </ul>
     </div>
 
-    <div class="paragraph">
-    <div class="notice">
-        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
-        Source code is available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
-    </div>
-    </div>
-
     <div class="paragraph center">
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
+        <img class="pc-screenshot" src="/img/screenshots/ss-whiteblocks-1.png" />
+        <img class="pc-screenshot" src="/img/screenshots/ss-whiteblocks-3.png" />
+        <img class="pc-screenshot" src="/img/screenshots/ss-whiteblocks-4.png" />
+        <img class="pc-screenshot" src="/img/screenshots/ss-whiteblocks-5.png" />
     </div>
     `, "#40B5AD", true, false),
+    
     new ProjectData("project-3", "Drawing Overload", "img/projects/project-3-icon.png", `
     <div class="paragraph">
         <strong>Drawing Overload</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
@@ -95,6 +91,7 @@ export default [
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
     </div>
     `, "#383838"),
+    
     new ProjectData("project-4", "Bunners", "img/projects/project-bunners-icon.gif", `
     <div class="paragraph">
         <strong>Bunners</strong> was the first commercial project I have worked on. 
